e82b3009bb335761b4e8659a65e3a66490287d98
pradojeric <jericprado0719@gmail.com>
2021-06-02 08:42:52 +0800

    new

--- app/Http/Controllers/API/ConductorController.php
+++ app/Http/Controllers/API/ConductorController.php
@@ -20,7 +20,7 @@ class ConductorController extends Controller
     function generateNumber()
     {
         $number = mt_rand(00000000000, 9999999999);
-        if(EmployeeRide::whereRideCode($number)->exists()) $this->generateNumber();
+        if (EmployeeRide::whereRideCode($number)->exists()) $this->generateNumber();
         return $number;
     }
 
@@ -31,15 +31,14 @@ class ConductorController extends Controller
             'password' => 'required',
         ]);
 
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return response()->json($validator->messages(), 422);
         }
 
         $credentials = request(['email', 'password']);
         $newCredentials = array_merge($credentials, ['active' => 1]);
 
-        if(!Auth::attempt($newCredentials)){
+        if (!Auth::attempt($newCredentials)) {
             return response()->json([
                 'message' => 'Given data is invalid',
                 'errors' => [
@@ -58,7 +57,6 @@ class ConductorController extends Controller
             'token' => $user->createToken($request->email)->plainTextToken,
             'profile' => $user->employeeProfile,
         ]);
-
     }
 
     public function logout(Request $request)
@@ -78,8 +76,7 @@ class ConductorController extends Controller
             'ride_id' => 'required|exists:rides,id',
         ]);
 
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return response()->json($validator->messages(), 422);
         }
 
@@ -115,8 +112,7 @@ class ConductorController extends Controller
             'ride_code' => 'required|exists:employee_ride,ride_code',
         ]);
 
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return response()->json($validator->messages(), 422);
         }
 
@@ -143,17 +139,17 @@ class ConductorController extends Controller
 
         $rides = Ride::join('buses', 'bus_id', 'buses.id')
             ->where('buses.conductor_id', $request->user()->id)
-            ->when($request->date, function($query) use ($travelDate, $dayName){
-                $query->where(function(Builder $query) use ($travelDate, $dayName){
+            ->when($request->date, function ($query) use ($travelDate, $dayName) {
+                $query->where(function (Builder $query) use ($travelDate, $dayName) {
                     $query->where('ride_date', $travelDate)
-                        ->orWhereHas('schedule', function(Builder $query) use ($travelDate, $dayName){
+                        ->orWhereHas('schedule', function (Builder $query) use ($travelDate, $dayName) {
                             $query->where($dayName, true)
-                                ->where(function(Builder $query) use ($travelDate){
-                                    $query->where('start_date', '<=' ,$travelDate);
-                                })->where(function(Builder $query) use ($travelDate){
+                                ->where(function (Builder $query) use ($travelDate) {
+                                    $query->where('start_date', '<=', $travelDate);
+                                })->where(function (Builder $query) use ($travelDate) {
                                     $query->where('end_date', '>=', $travelDate)
                                         ->orWhereNull('end_date');
-                            });
+                                });
                         });
                 });
             })
@@ -161,7 +157,7 @@ class ConductorController extends Controller
             ->with(['route', 'route.terminals', 'bus.driver.employeeProfile'])
             ->get();
 
-        $rides = $rides->map(function($ride){
+        $rides = $rides->map(function ($ride) {
             return ['ride' => $ride];
         });
 
@@ -176,23 +172,23 @@ class ConductorController extends Controller
 
         $ride = Ride::join('buses', 'bus_id', 'buses.id')
             ->where('buses.conductor_id', $request->user()->id)
-            ->where(function(Builder $query) use ($travelDate, $dayName){
+            ->where(function (Builder $query) use ($travelDate, $dayName) {
                 $query->where('ride_date', $travelDate)
-                    ->orWhereHas('schedule', function(Builder $query) use ($travelDate, $dayName){
+                    ->orWhereHas('schedule', function (Builder $query) use ($travelDate, $dayName) {
                         $query->where($dayName, true)
-                            ->where(function(Builder $query) use ($travelDate){
-                                $query->where('start_date', '<=' ,$travelDate);
-                            })->where(function(Builder $query) use ($travelDate){
+                            ->where(function (Builder $query) use ($travelDate) {
+                                $query->where('start_date', '<=', $travelDate);
+                            })->where(function (Builder $query) use ($travelDate) {
                                 $query->where('end_date', '>=', $travelDate)
                                     ->orWhereNull('end_date');
-                        });
+                            });
                     });
             })
             ->with(['route', 'route.terminals', 'bus.driver.employeeProfile'])
             ->selectRaw('*, rides.id as ride_id')
             ->first();
 
-        if(!$ride) return response()->json(['error' => 'No rides']);
+        if (!$ride) return response()->json(['error' => 'No rides']);
 
         $booked = Booking::where('ride_id', $ride->ride_id);
 
@@ -203,19 +199,19 @@ class ConductorController extends Controller
             ->where('ride_code', $request->ride_code)->first();
 
 
-        return response()->json(['ride' => $ride, 'booked' => $booked, 'aboard' => $aboard, 'exists' => $employeeRide] );
+        return response()->json(['ride' => $ride, 'booked' => $booked, 'aboard' => $aboard, 'exists' => $employeeRide]);
         // return response()->json(['ride' => $booked, 'error' => 'error'] );
     }
 
     public function getRide(Request $request, $id)
     {
         $ride = Ride::join('buses', 'bus_id', 'buses.id')
-            ->where('rides.id',$id)
+            ->where('rides.id', $id)
             ->selectRaw('*, rides.id as ride_id')
             ->with(['route', 'route.terminals', 'bus.driver.employeeProfile'])
             ->first();
 
-        if(!$ride) return response()->json(['error' => 'No rides']);
+        if (!$ride) return response()->json(['error' => 'No rides']);
 
         $booked = Booking::where('ride_id', $ride->id);
 
@@ -224,7 +220,7 @@ class ConductorController extends Controller
 
         $employeeRide = EmployeeRide::where('ride_code', $request->ride_code)->with(['departure', 'arrival'])->first();
 
-        return response()->json(['ride' => $ride, 'booked' => $booked, 'aboard' => $aboard, 'exists' => $employeeRide] );
+        return response()->json(['ride' => $ride, 'booked' => $booked, 'aboard' => $aboard, 'exists' => $employeeRide]);
     }
 
     public function getEmployeeProfile(Request $request)
@@ -237,27 +233,25 @@ class ConductorController extends Controller
 
         $booking = Booking::whereBookingCode($book_code);
 
-        if(!$booking->exists())
-        {
+        if (!$booking->exists()) {
             return response()->json(['error' => 'No bookings found!']);
         }
         $booking = $booking->first();
 
         $receipt = collect($booking->replicate()->only('booking_code', 'ride_id', 'passenger_id', 'start_terminal_id', 'end_terminal_id', 'pax'));
 
-        if($request->user()->company()->activate_point == 1)
-        {
+        if ($request->user()->company()->activate_point == 1) {
             $company_id = $request->user()->company()->id;
             $totalKm = $booking->ride->route->getTotalKm($booking->start_terminal_id, $booking->end_terminal_id);
-            $totalPoints = $totalKm/10 * $booking->ride->bus->busClass->point;
+            $totalPoints = $totalKm / 10 * $booking->ride->bus->busClass->point;
             $receipt = $receipt->merge(['points' => $totalPoints]);
             $booking->points = $totalPoints;
             //$booking->passenger->passengerProfile->points += $totalPoints;
             $passenger = $booking->passenger;
-            if(isset($booking->passenger->busPoints)){
+            if (isset($booking->passenger->busPoints)) {
                 $prev_points = $passenger->busPoints->find($company_id)->pivot->points;
                 $passenger->busPoints()->updateExistingPivot($company_id, ['points' => $prev_points + $totalPoints]);
-            }else{
+            } else {
                 $passenger->busPoints()->attach([$company_id => ['points' => $totalPoints]]);
             }
         }
@@ -279,25 +273,25 @@ class ConductorController extends Controller
         $end = Carbon::now()->addMonth();
         $rides = [];
 
-        for($i = $start; $i < $end; $i->addDay()){
+        for ($i = $start; $i < $end; $i->addDay()) {
             $date_string = $i->toDateString();
             $date = Carbon::parse($date_string);
             $dayName = Str::lower($date->dayName);
 
-            $rides[] = Ride::where(function(Builder $query) use ($date_string, $dayName){
+            $rides[] = Ride::where(function (Builder $query) use ($date_string, $dayName) {
                 $query->where('ride_date', $date_string)
-                    ->orWhereHas('schedule', function(Builder $query) use ($date_string, $dayName){
+                    ->orWhereHas('schedule', function (Builder $query) use ($date_string, $dayName) {
                         $query->where($dayName, true)
-                            ->where(function(Builder $query) use ($date_string){
-                                $query->where('start_date', '<=' ,$date_string);
-                            })->where(function(Builder $query) use ($date_string){
+                            ->where(function (Builder $query) use ($date_string) {
+                                $query->where('start_date', '<=', $date_string);
+                            })->where(function (Builder $query) use ($date_string) {
                                 $query->where('end_date', '>=', $date_string)
                                     ->orWhereNull('end_date');
-                        });
+                            });
                     });
-            });
+            })->pluck('id');
         }
 
-        return $rides;
+        return ['ride' => $rides];
     }
 }
