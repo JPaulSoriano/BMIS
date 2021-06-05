189110135c87ea9b0e7eec63b154c94bbbb41ea0
pradojeric <jericprado0719@gmail.com>
2021-06-05 10:29:48 +0800

    new

--- app/Http/Resources/PassengerProfile.php
+++ app/Http/Resources/PassengerProfile.php
@@ -15,6 +15,7 @@ class PassengerProfile extends JsonResource
     public function toArray($request)
     {
         return [
+            'user_name' => $this->user->name,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'contact' => $this->contact,
