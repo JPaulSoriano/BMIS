a9a7ea487c883fb0df7f536b13dd31f2100725d8
pradojeric <jericprado0719@gmail.com>
2021-06-07 14:35:32 +0800

    final_defense

--- resources/views/admin/rides/index.blade.php
+++ resources/views/admin/rides/index.blade.php
@@ -5,57 +5,61 @@
 
 <div class="container">
 
-<div class="row justify-content-center">
-    <div class="col-lg-12">
-    @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        {{ $message }}
-    </div>
-    @endif
-        <div class="card shadow mb-4">
-            <div class="card-header bg-primary d-sm-flex align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-white">Ride Schedules</h6>
-                <a class="btn btn-light btn-sm" href="{{ route('admin.rides.create') }}"><i class="fas fa-plus"></i></a>
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                {{ $message }}
             </div>
-            <div class="card-body">
-                <form action="#" method="get">
-                    <div class="row ml-auto">
-                        <input type="date" class="form-control col-sm-2" name="ride_date">
-                        <div class="form-check form-check-inline ml-2">
-                            <label class="form-check-label">
-                                <input class="form-check-input" type="radio" name="ride_type" value="all" checked> All
-                            </label>
-                            <label class="form-check-label ml-2">
-                                <input class="form-check-input" type="radio" name="ride_type" value="single" @if(request('ride_type') == 'single') checked @endif> Single
-                            </label>
-                            <label class="form-check-label ml-2">
-                                <input class="form-check-input" type="radio" name="ride_type" value="cyclic" @if(request('ride_type') == 'cyclic') checked @endif> Cyclic
-                            </label>
+            @endif
+            <div class="card shadow mb-4">
+                <div class="card-header bg-primary d-sm-flex align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-white">Ride Schedules</h6>
+                    <a class="btn btn-light btn-sm" href="{{ route('admin.rides.create') }}"><i
+                            class="fas fa-plus"></i></a>
+                </div>
+                <div class="card-body">
+                    <form action="#" method="get">
+                        <div class="row ml-auto">
+                            <input type="date" class="form-control col-sm-2" name="ride_date">
+                            <div class="form-check form-check-inline ml-2">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="radio" name="ride_type" value="all" checked>
+                                    All
+                                </label>
+                                <label class="form-check-label ml-2">
+                                    <input class="form-check-input" type="radio" name="ride_type" value="single"
+                                        @if(request('ride_type')=='single' ) checked @endif> Single
+                                </label>
+                                <label class="form-check-label ml-2">
+                                    <input class="form-check-input" type="radio" name="ride_type" value="cyclic"
+                                        @if(request('ride_type')=='cyclic' ) checked @endif> Cyclic
+                                </label>
+                            </div>
+                            <input type="submit" value="Search" class="btn btn-primary col-sm-auto ml-3">
+
                         </div>
-                        <input type="submit" value="Search" class="btn btn-primary col-sm-auto ml-3">
+                    </form>
 
-                    </div>
-                </form>
-
-                <div class="table-responsive mt-3">
-                    <table class="table table-bordered" width="100%" cellspacing="0" id="example">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Route</th>
-                                <th>Bus</th>
-                                <th>Departure Time</th>
-                                <th>Ride date</th>
-                                <th>Ride type</th>
-                                <th>Updated at</th>
-                                <th>State</th>
-                                <th style="width: 130px">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($rides as $ride)
+                    <div class="table-responsive mt-3">
+                        <table class="table table-bordered" width="100%" cellspacing="0" id="example">
+                            <thead>
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
+                                    <th>No</th>
+                                    <th>Route</th>
+                                    <th>Bus</th>
+                                    <th>Departure Time</th>
+                                    <th>Ride date</th>
+                                    <th>Ride type</th>
+                                    <th>Updated at</th>
+                                    <th>State</th>
+                                    <th style="width: 130px">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($rides as $ride)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
                                     <td>{{ $ride->route->route_name }}</td>
                                     <td>{{ $ride->bus->bus_name }}</td>
                                     <td>{{ $ride->departure_time_formatted }}</td>
@@ -64,25 +68,29 @@
                                     <td>{{ $ride->updated_at }}</td>
                                     <td>{{ $ride->isActive() ? 'Active' : 'Inactive' }}</td>
                                     <td class="d-flex justify-content-around">
-                                        <a class="btn btn-info btn-sm" href="{{ route('admin.rides.show', $ride) }}"><i class="fas fa-eye"></i></a>
-                                        <a class="btn btn-primary btn-sm" href="{{ route('admin.rides.edit', $ride) }}"><i class="fas fa-edit"></i></a>
+                                        <a class="btn btn-info btn-sm" href="{{ route('admin.rides.show', $ride) }}"><i
+                                                class="fas fa-eye"></i></a>
+                                        <a class="btn btn-primary btn-sm"
+                                            href="{{ route('admin.rides.edit', $ride) }}"><i
+                                                class="fas fa-edit"></i></a>
                                         <form action="{{ route('admin.rides.destroy', $ride) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
+                                            <button type="submit" class="btn btn-danger btn-sm"><i
+                                                    class="fas fa-trash-alt"></i></button>
                                         </form>
                                     </td>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $rides->links() }}
-                </div>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $rides->links() }}
+                    </div>
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 </div>
 
